c18172c27b56a8ba72f4f975686e89c40a43f123
Gary Lin 林品辰 <garylin2018@gmail.com>
2023-05-21 17:30:16 +0800

    Add margins to start screen

--- screens/onboarding/Start.tsx
+++ screens/onboarding/Start.tsx
@@ -11,9 +11,14 @@ import React from "react";
 const Start = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <Image source={require("../../assets/echodolphin.png")} />
-      <Text style={styles.welcomeText}>Welcome to EchoJournal</Text>
-      <Text>Reflect. Grow. Thrive</Text>
+      <Image
+        style={styles.dolphin}
+        source={require("../../assets/echodolphin.png")}
+      />
+      <View style={styles.textView}>
+        <Text style={styles.welcomeText}>Welcome to EchoJournal</Text>
+        <Text>Reflect. Grow. Thrive.</Text>
+      </View>
       <TouchableOpacity style={styles.button}>
         <Text style={styles.buttonText}>START</Text>
       </TouchableOpacity>
@@ -26,8 +31,16 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#859FAB",
     alignItems: "center",
-    justifyContent: "space-around",
-    gap: 10,
+    justifyContent: "flex-start",
+  },
+  dolphin: {
+    marginTop: "20%",
+  },
+  textView: {
+    marginTop: "25%",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 20,
   },
   welcomeText: {
     fontWeight: "400",
@@ -36,8 +49,10 @@ const styles = StyleSheet.create({
   mottoText: {
     fontWeight: "400",
     fontSize: 15,
+    fontFamily: "arimo",
   },
   button: {
+    marginTop: "40%",
     backgroundColor: "black",
     padding: 12,
     width: "60%",
