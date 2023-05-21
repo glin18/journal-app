b9e8d3253829c74511fa67472e189295913d5542
Gary Lin 林品辰 <garylin2018@gmail.com>
2023-05-21 15:42:18 +0800

    Add login title and textinputs

--- screens/Login.tsx
+++ screens/Login.tsx
@@ -1,10 +1,20 @@
-import { View, StyleSheet, Text, SafeAreaView } from "react-native";
+import { View, StyleSheet, Text, SafeAreaView, TextInput } from "react-native";
 import React from "react";
 
 const Login = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Log In</Text>
+      <Text style={styles.login}>Log In</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        placeholderTextColor="#19242E"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        placeholderTextColor="#19242E"
+      />
     </SafeAreaView>
   );
 };
@@ -12,8 +22,26 @@ const Login = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#859FAB",
     alignItems: "center",
+    gap: 30,
+  },
+  login: {
+    fontSize: 36,
+    marginTop: "20%",
+    marginBottom: "10%",
+  },
+  input: {
+    padding: 15,
+    width: "80%",
+    height: 55,
+    color: "#19242E",
+    borderRadius: 7,
+    backgroundColor: "#D9D9D9",
+    shadowColor: "#19242E",
+    shadowOffset: { width: 1, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
   },
 });
 
