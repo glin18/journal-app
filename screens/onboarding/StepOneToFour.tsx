c0b4b577e88f1832d363dfdd63f2fb9de9cb7f69
Gary Lin 林品辰 <garylin2018@gmail.com>
2023-05-21 18:17:46 +0800

    Create basic layout for step 1

--- screens/onboarding/StepOneToFour.tsx
+++ screens/onboarding/StepOneToFour.tsx
@@ -1,12 +1,45 @@
-import { View, Text } from "react-native";
+import { View, Text, SafeAreaView, StyleSheet } from "react-native";
 import React from "react";
+import Button from "../../components/onboarding/Button";
 
 const StepOneToFour = () => {
   return (
-    <View>
-      <Text>StepOne</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.mainText}>Walkthrough Step 1</Text>
+      <Text style={styles.secondaryText}>
+        <Text style={styles.bold}>Create Your Journal Entry:</Text> Tap on the
+        "+" button to start your journal entry. Choose your mood, category, and
+        input method (voice or written) to capture your thoughts with ease and
+        authenticity.
+      </Text>
+      <Button>Next</Button>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#859FAB",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    gap: 30,
+  },
+  mainText: {
+    fontWeight: "400",
+    fontSize: 24,
+    width: "80%",
+    textAlign: "center",
+  },
+  secondaryText: {
+    fontWeight: "400",
+    fontSize: 16,
+    width: "75%",
+    marginBottom: "40%",
+  },
+  bold: {
+    fontWeight: "600",
+  },
+});
 
 export default StepOneToFour;
