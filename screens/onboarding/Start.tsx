99284a6e4339840b2a5717340419bc3eb9da5311
Gary Lin 林品辰 <garylin2018@gmail.com>
2023-05-21 17:15:56 +0800

    Add background, welcome text, motto text

--- screens/onboarding/Start.tsx
+++ screens/onboarding/Start.tsx
@@ -1,12 +1,31 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, SafeAreaView, Image } from "react-native";
 import React from "react";
 
 const Start = () => {
   return (
-    <View>
-      <Text>Start</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Image source={require("../../assets/echodolphin.png")} />
+      <Text style={styles.welcomeText}>Welcome to EchoJournal</Text>
+      <Text>Reflect. Grow. Thrive</Text>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#859FAB",
+    alignItems: "center",
+    gap: 10,
+  },
+  welcomeText: {
+    fontWeight: "400",
+    fontSize: 24,
+  },
+  mottoText: {
+    fontWeight: "400",
+    fontSize: 15,
+  },
+});
 
 export default Start;
